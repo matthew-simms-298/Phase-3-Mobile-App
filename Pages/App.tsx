5b24d2ba5839a877a467d3422415e1d1735bb2f0
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2023-10-27 13:12:02 -0600

    Initial Commit.
    Ok sorry for the wait, HERE WE GO

---
+++ Pages/App.tsx
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+      <TextInput>Enter Username:</TextInput>
+      <TextInput>Enter Password:</TextInput>
+    </View>
+  );
+}
+
+export default App;
---
+++ Pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function HomePage(): JSX.Element {
+    return (
+      <View>
+        <Text>Testing Hello</Text>
+      </View>
+    );
+  }
+
+export default HomePage
